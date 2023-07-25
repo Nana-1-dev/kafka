2ef3420a4b413096c77a8bcb7c79050e02c0632a
Nana-1-dev <132481925+Nana-1-dev@users.noreply.github.com>
2023-07-25 09:26:58 +0100

    premier commit

---
+++ src/test/java/net/supermarches/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.supermarches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
